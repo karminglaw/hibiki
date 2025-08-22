175c657026a9e10599626edc78514854e323a729
karminglaw <karming021@gmail.com>
2025-08-23 02:33:38 +0800

    fix missing image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import HomeBanner from '@/components/layouts/HomeBanner.vue'
 <template>
   <HomeBanner />
   <ContentWImage
-    :src="'/src/assets/contentimage.jpg'"
+    :src="'/contentimage.jpg'"
     :subheader-text="'Our Story'"
     :content-text="'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'"
   />
